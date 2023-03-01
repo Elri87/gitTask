72af8f29153c9d4e533ee9fe9acc00864be111c9
Elri <87elrivdm@gmail.com>
2023-03-01 13:50:16 +0200

    Changes I made to this brunch

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,11 @@
-function sayHello() {
-    console.log("Git is Awesome!");
+/*function sayHello() {
+    console.log(promt("Say Git is Awesome!"));
   }
 
-  sayHello();
+  sayHello();*/
+
+  function promptUserToSaySomething() {
+    let userInput = prompt("Please say something:");
+    console.log("You said: " + userInput);
+  }
+  promptUserToSaySomething();
